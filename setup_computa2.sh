e8d97e9b5c29656077004cfa3355e21982e2895b
Tyrone Nicholas <tyrone.nicholas@gmail.com>
2025-06-20 14:52:41 +0100

    Fix install scripts

--- setup_computa2.sh
+++ setup_computa2.sh
@@ -1,4 +1,6 @@
 cd ~/MLXTwoTowers
 pyenv install 3.13.3
-pip install -r requirements.txt
+pip install --upgrade --break-system-packages -r requirements.txt
+CUDA_TAG=$(nvidia-smi | grep -oP 'CUDA Version: \K[0-9]+\.[0-9]+' | head -1 | tr -d .); \
+nvidia-smi && pip install --upgrade --index-url https://download.pytorch.org/whl/cu${CUDA_TAG} torch
 mkdir data/
